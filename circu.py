2d758d1cf1b9b051edd8208d0af60c04a9298fc5
noreen-n <164480106+noreen-n@users.noreply.github.com>
2024-03-29 17:19:26 +0100

    Add files via upload

---
+++ circu.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 15:47:03 2024
+
+@author: noreenchau
+"""
+
+def circu(u, v, x, y):
+    
+
---
+++ pressure.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 15:47:51 2024
+
+@author: noreenchau
+"""
+
+rho = 1000
+g = 9,81
+
+import numpy as np 
+
+def pressure(rho, g, u, v, C):
+    
+    p = np.zeros((u.shape[0], u.shape[1]))
+    
+    for i in range (1, u.shape[0]-1):
+        for j in range (1, u.shape[1]-1):
+           p[i][j] = rho*g*(C - (u[i][j]**2 + v[i][j]**2)/2*g)
+    
+    
+    
+    return p
+
+# appeler velocity pour u et v
