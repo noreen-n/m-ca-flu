b69105be6a391caedff2fd31eb242703c84d8553
noreen-n <164480106+noreen-n@users.noreply.github.com>
2024-04-17 12:10:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ from force import force
 from defineCl import createCl
 from defineCl import borders
 
-from deriv import deriv
-
 rho = 1000
 g = 9.81
 C =0
@@ -37,29 +35,37 @@ def main(cas):
         p = pressure(rho, g, u, v, C)
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(phi, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(phi, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Fonction de courant")
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(u, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(u, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Vitesse u")
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(v, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(v, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Vitesse v")
         
-        fig,ax = plt.subplots()
-        dimension = u.shape[0]
-        m = np.arange(0, dimension, 1)
-        n = np.arange(0, dimension, 1)
+        fig,ax = plt.subplots(figsize=(3, 7))
+        plt.gca().invert_yaxis()
+        plt.gca().invert_yaxis()
+        dimension1 = u.shape[0]
+        dimension2 = u.shape[1]
+        m = np.arange(0, dimension1*h, h)
+        n = np.arange(0, dimension2*h, h)
+        N, M = np.meshgrid(n, m)
         plt.title("Lignes de courant")
-        strm=ax.streamplot(m, n, u, v, 1)
+        strm=ax.streamplot(N, M, v, u, 1)
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(p, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(p, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Pression")
         
@@ -70,14 +76,9 @@ def main(cas):
         dom = np.loadtxt('2-dom.txt', dtype = int)
         num = np.loadtxt('2-num.txt', dtype = int)
         x,y = borders(dom)
-        cl = createCl(dom, x, y, 3.5)
+        cl = createCl(dom, 3.5)
         
         phi = Laplace(dom, num, cl)
-        
-        fig,ax = plt.subplots()
-        p1 = ax.imshow(phi, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='Blues')
-        plt.colorbar(p1)
-        plt.title("Fonction de courant")
         
         u,v = velocity(phi, dom, h)
         p = pressure(rho, g, u, v, C)
@@ -96,30 +97,42 @@ def main(cas):
                 #yy[index-1] = j
                 uArray.insert(index-1, u[i][j])
                 vArray.insert(index-1, v[i][j])
-                pArray.insert(index-1, u[i][j])
+                pArray.insert(index-1, p[i][j])
         
         circu(uArray, vArray, x, y)
         force(pArray, x, y)
+                
+        fig,ax = plt.subplots()
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(phi, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
+        plt.colorbar(p1)
+        plt.title("Fonction de courant")
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(u, extent=(0, 250, 1450, 0))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(u, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Vitesse u")
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(v, extent=(0, 250, 1450, 0))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(v, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Vitesse v")
-        
-        fig,ax = plt.subplots()
-        dimension = u.shape[0]
-        m = np.arange(0, dimension, 1)
-        n = np.arange(0, dimension, 1)
+    
+        fig,ax = plt.subplots(figsize=(3, 7))
+        plt.gca().invert_yaxis()
+        dimension1 = u.shape[0]
+        dimension2 = u.shape[1]
+        m = np.arange(0, dimension1*h, h)
+        n = np.arange(0, dimension2*h, h)
+        N, M = np.meshgrid(n, m)
         plt.title("Lignes de courant")
-        strm=ax.streamplot(m, n, u, v, 1)
+        strm=ax.streamplot(N, M, v, u, 1)
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(p, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(p, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Pression")
         
@@ -130,36 +143,43 @@ def main(cas):
         dom = np.loadtxt('3-dom.txt', dtype = int)
         num = np.loadtxt('3-num.txt', dtype = int)
         x,y = borders(dom)
-        cl = createCl(dom, x, y, 3.5)
+        cl = createCl(dom, 3.5)
         
         phi = Laplace(dom, num, cl)
         u,v = velocity(phi, dom, h)
         p = pressure(rho, g, u, v, C)
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(phi, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(phi, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Fonction de courant")
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(u, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(u, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Vitesse u")
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(v, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(v, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Vitesse v")
         
-        fig,ax = plt.subplots()
-        dimension = u.shape[0]
-        m = np.arange(0, dimension, 1)
-        n = np.arange(0, dimension, 1)
+        fig,ax = plt.subplots(figsize=(3, 7))
+        plt.gca().invert_yaxis()
+        dimension1 = u.shape[0]
+        dimension2 = u.shape[1]
+        m = np.arange(0, dimension1*h, h)
+        n = np.arange(0, dimension2*h, h)
+        N, M = np.meshgrid(n, m)
         plt.title("Lignes de courant")
-        strm=ax.streamplot(m, n, u, v, 1)
+        strm=ax.streamplot(N, M, v, u, 1)
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(p, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(p, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Pression")
         
@@ -179,91 +199,46 @@ def main(cas):
             y[i] = contour[i][1]
             
         # prendre x et y par rapport au doc contour
-        cl = createCl(dom, x, y, 35)
+        cl = createCl(dom, 3.5)
         
         phi = Laplace(dom, num, cl)
         u,v = velocity(phi, dom, h)
         p = pressure(rho, g, u, v, C)
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(phi, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(phi, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Fonction de courant")
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(u, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(u, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Vitesse u")
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(v, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(v, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Vitesse v")
         
-        fig,ax = plt.subplots()
-        dimension = u.shape[0]
-        m = np.arange(0, dimension, 1)
-        n = np.arange(0, dimension, 1)
+        fig,ax = plt.subplots(figsize=(3, 7))
+        plt.gca().invert_yaxis()
+        dimension1 = u.shape[0]
+        dimension2 = u.shape[1]
+        m = np.arange(0, dimension1*h, h)
+        n = np.arange(0, dimension2*h, h)
+        N, M = np.meshgrid(n, m)
         plt.title("Lignes de courant")
-        ax.streamplot(m, n, u, v, 1)
+        strm=ax.streamplot(N, M, v, u, 1)
         
         fig,ax = plt.subplots()
-        p1 = ax.imshow(p, extent=(0, 4, 0, 4))
+        plt.gca().invert_yaxis()
+        p1 = ax.imshow(p, extent=(0, np.shape(cl)[1]*h, np.shape(dom)[0]*h, 0), cmap='inferno')
         plt.colorbar(p1)
         plt.title("Pression")
         
-main(2)
-
-        
+main(4)
 
 
-"""uArray = []
-vArray = [] 
-
-dim = np.max(num)
-x = np.zeros(dim) 
-y = np.zeros(dim)
-pArray = []
-
-for i in range (1, num.shape[0]-1):
-    for j in range (1, num.shape[1]-1):
-        index = num[i][j]
-        x[index-1] = i
-        y[index-1] = j
-        uArray.insert(index-1, u[i][j])
-        vArray.insert(index-1, v[i][j])
-        pArray.insert(index-1, u[i][j])
-        
-#xArray = np.array(x)
-print("x")
-print(x)
-#yArray = np.array(y)
-print("y")
-print(y)
-
-        
-circu(uArray, vArray, x, y)
-force(pArray, x, y)
-
-fig,ax = plt.subplots()
-
-#plt.subplot(1, 3, 1)
-#plt.plot(u)
-#plt.title("u")
-#plt.subplot(1, 3, 2)
-#plt.plot(v)
-#plt.title("v")
-#plt.subplot(1,3,3)
-#plt.plot(p)
-#plt.title("p")
-
-dimension = u.shape[0]
-
-m = np.arange(0, dimension, 1)
-n = np.arange(0, dimension, 1)
-
-#p2 = ax.imshow(u)
-strm=ax.streamplot(m, n, u, v, 1) #plus pour vitesse
-#fig.colorbar(p2)
-#fig.color(p2)
-# ou strm à la place de p2"""
